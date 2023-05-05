e74482bcf5ff4ba7d49f11ace466d39062fde2e4
Bram van Deventer <bram2202@gmail.com>
2023-05-05 18:52:47 +0200

    disable import-error

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 """Models for the application."""
 
 from enum import Enum
-from pydantic import BaseModel # pylint: disable=no-name-in-module
+from pydantic import BaseModel # pylint: disable=no-name-in-module, import-error
 
 
 class SensorType(Enum): # pylint: disable=too-few-public-methods
