310de8c6ef7e1f319c4d5cbd4f776c35499babfe
Bram van Deventer <bram2202@gmail.com>
2023-06-08 19:19:28 +0200

    Move tests

--- src/ha_api.py
+++ src/ha_api.py
@@ -16,8 +16,8 @@ class HomeAssistantApi:  # pylint: disable=too-few-public-methods
         config.sections()
         config.read("settings.ini")
 
-        self.api_url = os.environ['api_url'] #config["home_assistant"]["api_url"]
-        self.api_token = os.environ['api_token'] #config["home_assistant"]["api_token"]
+        self.api_url = os.environ['api_url']
+        self.api_token = os.environ['api_token']
 
     def get_states(self) -> list[models.HaSensor]:
         """Get all states from Home Assistant."""
